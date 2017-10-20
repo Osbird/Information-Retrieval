e97de1471173cfab622c6674b342fa9f613ca927
Magnus MO <magnusosb@gmail.com>
2017-10-20 12:59:18 +0200

    Filtering paragraphs

--- Ov3/Oving3_Gensim.py
+++ Ov3/Oving3_Gensim.py
@@ -3,12 +3,20 @@ import codecs
 
 
 partitionedFile = []
-linenum = int
+#Loading file and partitioneing in an array of paragraphs
 try:
     f = codecs.open("theBook_Oving3.txt", "r", "utf-8");
     entirefile = f.read()
     partitionedFile = entirefile.split('\n')
 except FileNotFoundError:
     print("whoops, that file does not seem to exist")
-print(partitionedFile)
 print(len(partitionedFile))
+
+
+#Filter the array to exclude elements containing the word "Guthenberg"
+filteredList = [element for element in partitionedFile if 'Gutenberg' not in element]
+print("Filtrert liste")
+print(len(filteredList))
+
+#Tokenize the paragraphs. We now have a list of paragrahs, where the paragraphs are lists of words
+for
