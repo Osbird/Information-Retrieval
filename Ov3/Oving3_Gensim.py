6b17a8926fb9d3904dd8e9cab48791dace645108
Magnus MO <magnusosb@gmail.com>
2017-10-20 13:51:40 +0200

    Change partitionedFile to filteredList

--- Ov3/Oving3_Gensim.py
+++ Ov3/Oving3_Gensim.py
@@ -1,12 +1,8 @@
 import random; random.seed(123)
 import codecs
-<<<<<<< HEAD
 import string
-
-print (string.punctuation+"\n\r\t")
-=======
 import nltk
->>>>>>> d722d2c94695a85f9b2398e94152ea5ebc0ca3ab
+
 
 
 #Loading file and partitioneing in an array of paragraphs
@@ -25,16 +21,12 @@ print("Filtrert liste")
 
 print(len(filteredList))
 
-partitionedFile = [paragraph.translate(string.punctuation).lower() for paragraph in partitionedFile]
-
-
-
-
-
+filteredList = [paragraph.translate(string.punctuation).lower() for paragraph in filteredList]
 
 print (partitionedFile[-5:])
 
-#Tokenize the paragraphs. We now have a list of paragrahs, where the paragraphs are lists of words
 #Tokenize the paragraphs. We now have a list of paragraphs, where the paragraphs are lists of words
 for i in range(len(filteredList)):
     filteredList[i] = nltk.word_tokenize(filteredList[i])
+
+
