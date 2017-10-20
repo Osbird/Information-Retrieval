072835425ad3d33a967b653c823e37c06eca7277
haraldvinje <haraldvinje@gmai.com>
2017-10-20 13:55:44 +0200

    changed name of variable, and stripped paragraphs of certain chars

--- Ov3/Oving3_Gensim.py
+++ Ov3/Oving3_Gensim.py
@@ -1,12 +1,10 @@
 import random; random.seed(123)
 import codecs
-<<<<<<< HEAD
 import string
-
-print (string.punctuation+"\n\r\t")
-=======
 import nltk
->>>>>>> d722d2c94695a85f9b2398e94152ea5ebc0ca3ab
+from nltk.stem.porter import PorterStemmer
+import gensim
+
 
 
 #Loading file and partitioneing in an array of paragraphs
@@ -25,16 +23,10 @@ print("Filtrert liste")
 
 print(len(filteredList))
 
-partitionedFile = [paragraph.translate(string.punctuation).lower() for paragraph in partitionedFile]
+filteredList = [paragraph.translate(string.punctuation).lower().stem() for paragraph in filteredList]
 
 
 
-
-
-
-print (partitionedFile[-5:])
-
-#Tokenize the paragraphs. We now have a list of paragrahs, where the paragraphs are lists of words
 #Tokenize the paragraphs. We now have a list of paragraphs, where the paragraphs are lists of words
 for i in range(len(filteredList)):
     filteredList[i] = nltk.word_tokenize(filteredList[i])
