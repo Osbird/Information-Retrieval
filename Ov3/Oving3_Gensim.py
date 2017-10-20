ad8f719cf2bd0e6918b85a97d121a068c2cd5a3e
Magnus MO <magnusosb@gmail.com>
2017-10-20 12:46:52 +0200

    partitioned file function completed

--- Ov3/Oving3_Gensim.py
+++ Ov3/Oving3_Gensim.py
@@ -6,28 +6,9 @@ partitionedFile = []
 linenum = int
 try:
     f = codecs.open("theBook_Oving3.txt", "r", "utf-8");
-    read = f.readline()
-    temporaryString = ""
-    while read != '':
-        if read == "\n":
-            print(1)
-            partitionedFile.append(temporaryString)
-            temporaryString = ""
-
-        else:
-            temporaryString = temporaryString + "\n" + read
-            #print(temporaryString)
-
-        print(read)
-        read=f.readline()
-
+    entirefile = f.read()
+    partitionedFile = entirefile.split('\n')
 except FileNotFoundError:
     print("whoops, that file does not seem to exist")
-
-
 print(partitionedFile)
-
-
-g = codecs.open("theBook_Oving3.txt", "r", "utf-8");
-entirefile = g.read()
-paragr = entirefile.split('\s{4,}')
+print(len(partitionedFile))
